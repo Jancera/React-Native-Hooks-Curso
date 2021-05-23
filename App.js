f3f2bc9b0a604f22da94487c97083d02b4250b2b
Jancer Lima <jancerboy@gmail.com>
2021-05-22 21:11:51 -0300

    Aula 2

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-<<<<<<< HEAD
 import { StyleSheet, Text, View, Button } from "react-native";
 
 const Pessoa = (props) => {
@@ -12,20 +11,14 @@ const Pessoa = (props) => {
     </View>
   );
 };
-=======
-import { StyleSheet, Text, View } from "react-native";
->>>>>>> 9834b0440d614ab3a0348c6b08b62d90bb2e411c
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
       <StatusBar style="auto" />
-<<<<<<< HEAD
       <Button title="Teste" />
       <Pessoa name="Alex" sobrenome="Lima" />
-=======
->>>>>>> 9834b0440d614ab3a0348c6b08b62d90bb2e411c
     </View>
   );
 }
