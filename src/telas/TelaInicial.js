cd2de2e813f817721e06201820f1547fd7f5900a
Jancera <jancerboy@gmail.com>
2021-05-29 12:59:47 -0300

    Aula 5

--- src/telas/TelaInicial.js
+++ src/telas/TelaInicial.js
@@ -4,8 +4,10 @@ import { View, Text, StyleSheet, Button } from "react-native";
 const TelaInicial = (props) => {
   console.log(props);
   return (
-    <View>
-      <Text>Inicial</Text>
+    <View style={styles.view}>
+      <Text style={StyleSheet.compose(styles.test1, styles.test2)}>
+        Inicial
+      </Text>
       <Button
         title="Navegar"
         onPress={() => props.navigation.navigate("Primeira")}
@@ -14,6 +16,25 @@ const TelaInicial = (props) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    borderWidth: 5,
+    borderColor: "blue",
+  },
+  text: {
+    fontSize: 20,
+    margin: 20,
+    borderWidth: 3,
+    borderColor: "red",
+    backgroundColor: "#BFBFBF",
+  },
+  test1: {
+    borderWidth: 3,
+  },
+  test2: {
+    borderColor: "red",
+  },
+});
 
 export default TelaInicial;
