68c02fea9e3ad0d898d3c950947c8caa40888535
Jancera <jancerboy@gmail.com>
2021-07-06 12:18:54 -0300

    Aula 15.2

--- src/component/Imagem.js
+++ src/component/Imagem.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from "react-native";
 
-const Imagem = ({ texto }) => {
-  return <Text>{texto}</Text>;
+const { width } = Dimensions.get("window");
+
+const Imagem = ({ texto, img }) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.imagem} source={img} />
+      <Text style={styles.texto}>{texto}</Text>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { marginBottom: 20 },
+  imagem: {
+    width: width - 20,
+    height: width / (16 / 9),
+    alignSelf: "center",
+    borderRadius: 20,
+  },
+  texto: {
+    position: "absolute",
+    color: "white",
+    fontSize: 30,
+    padding: 20,
+    textShadowColor: "black",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 10,
+  },
+});
 
 export default Imagem;
--- src/telas/CR.js
+++ src/telas/CR.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 import Imagem from "../component/Imagem";
 
 const CR = () => {
   return (
-    <View>
-      <Imagem texto="Imagem 1" />
-      <Imagem texto="Imagem 2" />
-      <Imagem texto="Imagem 3" />
-    </View>
+    <ScrollView>
+      <Imagem
+        texto="Lago di Brales "
+        img={require("../../assets/images/1.png")}
+      />
+      <Imagem
+        texto="Pragser Wildsee "
+        img={require("../../assets/images/2.png")}
+      />
+      <Imagem
+        texto="Yosemite Valley "
+        img={require("../../assets/images/3.png")}
+      />
+      <Imagem
+        texto="Sorapiss "
+        img={{
+          uri: "https://images.unsplash.com/photo-1508144753681-9986d4df99b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
+        }}
+      />
+    </ScrollView>
   );
 };
 
