47e241202e614638d8d51b52516ddb38dc5c1044
Jancera <jancerboy@gmail.com>
2021-07-28 12:56:27 -0300

    Aula 20

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TelaInicial from "./src/telas/TelaInicial";
+import Resultados from "./src/telas/Resultados";
+import Detalhes from "./src/telas/Detalhes";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="TelaInicial"
+          component={TelaInicial}
+        />
+        <Stack.Screen
+          name="Resultados"
+          component={Resultados}
+        />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Stack.Screen
+          name="Detalhes"
+          component={Detalhes}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/telas/TelaInicial.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const TelaInicial = () => {
+  return (
+    <View>
+      <Text>TelaInicial</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TelaInicial;
