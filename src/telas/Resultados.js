b7c5f644d94f2aa78a79d3517489bd0a39a475aa
Jancer Lima <contato.jancersl@gmail.com>
2021-08-16 01:17:39 -0300

    Aula 20.8

--- src/telas/Resultados.js
+++ src/telas/Resultados.js
@@ -9,15 +9,22 @@ import {
   Image,
   Dimensions,
   TouchableOpacity,
+  Text,
+  View,
 } from "react-native";
+import { Ionicons } from "react-native-vector-icons";
 import Cabecalho from "../components/Cabecalho";
 import axios from "axios";
 import API_KEY from "../API_KEY";
+import TextoInfo from "../components/TextoInfo";
+import Loading from "../components/Loading";
 
 const Resultados = ({ navigation, route }) => {
   const escolha = route.params.escolha;
   const link = `https://api.giphy.com/v1/${escolha}/search`;
   const [text, setText] = useState("");
+  const [showMessage, setShowMessage] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [data, setData] = useState([]);
 
@@ -57,6 +64,12 @@ const Resultados = ({ navigation, route }) => {
           data={data}
           keyExtractor={(element) => element.id}
           numColumns={2}
+          ListHeaderComponent={
+            <>
+              <TextoInfo showMessage={showMessage} />
+              <Loading isLoading={isLoading} />
+            </>
+          }
           renderItem={({ item }) => {
             return (
               <TouchableOpacity
